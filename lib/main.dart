88af547602e8236e08eff9eefac25b5ee7119ae2
“berlinen” <1292393108@qq.com>
2020-11-19 11:27:06 +0800

    feat: MyappBar

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,38 @@ void main() {
   );
 }
 
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  // Widget子类中的字段往往都会定义为"final"
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      height: 56.0, // 单位是逻辑上的像素（并非真实的像素，类似于浏览器中的像素）
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: new BoxDecoration(color: Colors.blue[500]),
+      // Row 是水平方向的线性布局（linear layout）
+      child: new Row (
+        // Row 是水平方向的线性布局（linear layout）
+        children: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.menu),
+            tooltip: 'navigation menu',
+            onPressed: null, // null 会禁用button
+          ),
+          new Expanded(child: title,),
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'search',
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
