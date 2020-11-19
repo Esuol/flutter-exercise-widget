ac99ebee3680dbbdadcc313fb7e9ba15db99a0da
“berlinen” <1292393108@qq.com>
2020-11-19 15:15:30 +0800

    feat: 头部

--- lib/sample_page.dart
+++ lib/sample_page.dart
@@ -45,7 +45,35 @@ class SamplePageContent extends StatelessWidget {
           // Line 2
           new Row(
             children: <Widget>[
-              
+              new Container(
+                width: 52.0,
+                height: 52.0,
+                margin: const EdgeInsets.only(left: 16.0),
+                child: new CircleAvatar(
+                  backgroundImage: new AssetImage('assets/images/avatar2.png'),
+                )
+              ),
+
+              new Container(
+                margin: const EdgeInsets.only(left: 7.0, right: 15.0),
+                height: 48.0,
+                child: new Stack(
+                  children: <Widget>[
+                    new Image.asset('assets/images/publish_chat_box.png'),
+                    new Positioned(
+                      left: 25.0,
+                      top: 14.0,
+                      child: new Text(
+                        "张老师发布了一个任务，请查收",
+                        style: new TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.black
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
           // Line 3
