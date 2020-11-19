163666b8f6b266b9309257223ffdfaa3b0a90243
“berlinen” <1292393108@qq.com>
2020-11-19 11:32:05 +0800

    feat: myScaffold

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyAppBar extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 8.0),
       decoration: new BoxDecoration(color: Colors.blue[500]),
       // Row 是水平方向的线性布局（linear layout）
-      child: new Row (
+      child: new Row(
         // Row 是水平方向的线性布局（linear layout）
         children: <Widget>[
           new IconButton(
@@ -33,7 +33,9 @@ class MyAppBar extends StatelessWidget {
             tooltip: 'navigation menu',
             onPressed: null, // null 会禁用button
           ),
-          new Expanded(child: title,),
+          new Expanded(
+            child: title,
+          ),
           new IconButton(
             icon: new Icon(Icons.search),
             tooltip: 'search',
@@ -45,4 +47,27 @@ class MyAppBar extends StatelessWidget {
   }
 }
 
-
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // // Material 是UI呈现的“一张纸”
+    return new Material(
+      // Column is 垂直方向布局
+      child: new Column(
+        children: <Widget>[
+          new MyAppBar(
+            title: new Text(
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.title
+            ),
+          ),
+          new Expanded(
+            child: new Center(
+              child: new Text('hello, world'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
