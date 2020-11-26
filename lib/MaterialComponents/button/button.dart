2719e3844c36d242189699fd673d002093f85938
“berlinen” <1292393108@qq.com>
2020-11-26 20:30:45 +0800

    feat: grad raisebutton

--- lib/MaterialComponents/button/button.dart
+++ lib/MaterialComponents/button/button.dart
@@ -12,7 +12,27 @@ class CustomButtonScreen extends StatelessWidget {
           const RaisedButton(
             onPressed: null,
             child: Text('Disabled button', style: TextStyle(fontSize: 20)),
-          )
+          ),
+          const SizedBox(height: 30),
+          RaisedButton(
+            onPressed: () {},
+            textColor: Colors.white,
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                 gradient: LinearGradient(
+                  colors: <Color>[
+                    Color(0xFF0D47A1),
+                    Color(0xFF1976D2),
+                    Color(0xFF42A5F5),
+                  ],
+                ),
+              ),
+              padding: const EdgeInsets.all(10.0),
+              child:
+                  const Text('Gradient Button', style: TextStyle(fontSize: 20)),
+            ),
+          ),
         ],
       ),
     );
