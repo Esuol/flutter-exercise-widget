bef666b5b61439c577a4eecba4d3b555f14cea36
“berlinen” <1292393108@qq.com>
2020-11-28 14:29:33 +0800

    feat: enable raisedButton;

--- lib/MaterialComponents/button/button.dart
+++ lib/MaterialComponents/button/button.dart
@@ -16,11 +16,16 @@ class CustomButtonScreen extends StatelessWidget {
           const SizedBox(height: 30),
           RaisedButton(
             onPressed: () {},
+            child: const Text('Enabled Button', style: TextStyle(fontSize: 20)),
+          ),
+          const SizedBox(height: 30),
+          RaisedButton(
+            onPressed: () {},
             textColor: Colors.white,
             padding: const EdgeInsets.all(0.0),
             child: Container(
               decoration: const BoxDecoration(
-                 gradient: LinearGradient(
+                gradient: LinearGradient(
                   colors: <Color>[
                     Color(0xFF0D47A1),
                     Color(0xFF1976D2),
@@ -29,8 +34,7 @@ class CustomButtonScreen extends StatelessWidget {
                 ),
               ),
               padding: const EdgeInsets.all(10.0),
-              child:
-                  const Text('Gradient Button', style: TextStyle(fontSize: 20)),
+              child: const Text('Gradient Button', style: TextStyle(fontSize: 20)),
             ),
           ),
         ],
