50fcd784fd70b3a221252efe46f376cb449e5ffe
“berlinen” <1292393108@qq.com>
2020-11-28 15:22:32 +0800

    feat: TextFieldState

--- lib/MaterialComponents/Input/textFiled.dart
+++ lib/MaterialComponents/Input/textFiled.dart
@@ -9,8 +9,52 @@ class TextFieldScreen extends StatelessWidget {
         TextField(
           obscureText: true,
           decoration: InputDecoration(border: OutlineInputBorder(), labelText: 'password'),
-        )
+        ),
       ],
+    );
+  }
+}
+
+class TextFieldStateScreen extends StatefulWidget {
+  TextFieldStateScreen({Key key}) : super(key: key);
+
+  @override
+  _TextState createState() => _TextState();
+}
+
+class _TextState extends State<TextFieldStateScreen> {
+  TextEditingController _controller;
+
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: TextField(
+          controller: _controller,
+          onSubmitted: (String value) async {
+            await showDialog<void>(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('thanks!'),
+                  content: Text('you typed "$value". '),
+                  actions: <Widget>[
+                    FlatButton(
+                      onPressed: () => {Navigator.pop(context)},
+                      child: Text('ok'),
+                    )
+                  ],
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class CustomPage extends StatelessWidget {
     return new Scaffold(
         appBar: new AppBar(title: new Text('组件库练习')),
         body: new Center(
-          child: new TextFieldScreen(),
+          child: new TextFieldStateScreen(),
         ));
     // body: DefaultTabController(
     //   length: 3,
