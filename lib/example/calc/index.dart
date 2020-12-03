0bad3b90fce87e910c62b4089a09e4b09bacd1fb
“berlinen” <1292393108@qq.com>
2020-12-03 11:34:21 +0800

    feat: 计数器

--- lib/example/calc/index.dart
+++ lib/example/calc/index.dart
@@ -9,5 +9,39 @@ class CalcScreen extends StatefulWidget {
 }
 
 class _CalcState extends State<CalcScreen> {
-  
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: new Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 }
