846af06a7fa921f6f4219eda29e1f4ab776145e3
“berlinen” <1292393108@qq.com>
2020-12-03 11:33:50 +0800

    feat: 计数wrapper

---
+++ lib/example/calc/index.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CalcScreen extends StatefulWidget {
+  CalcScreen({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _CalcState createState() => new _CalcState();
+}
+
+class _CalcState extends State<CalcScreen> {
+  
+}
