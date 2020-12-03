bb41668acac239ba0cfb7ce16b736dd5328668ac
“berlinen” <1292393108@qq.com>
2020-12-03 11:38:56 +0800

    feat: route example

--- lib/example/router/index.dart
+++ lib/example/router/index.dart
@@ -13,3 +13,24 @@ class RouteScreen extends StatelessWidget {
     );
   }
 }
+
+// 在_MyHomePageState.build方法中的Column的子widget中添加一个按钮（FlatButton） :
+
+class _MyHomePageState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Text("open new route"),
+        textColor: Colors.blue,
+        onPressed: () {
+          //导航到新路由
+          Navigator.push( context,
+           MaterialPageRoute(builder: (context) {
+              return RouteScreen();
+           }));
+          },
+      ),
+    );
+  }
+}
