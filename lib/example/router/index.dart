00cb1514105da999adf34f981108ce5e181b8a37
“berlinen” <1292393108@qq.com>
2020-12-03 11:49:30 +0800

    feat: route注释

--- lib/example/router/index.dart
+++ lib/example/router/index.dart
@@ -34,3 +34,17 @@ class _MyHomePageState extends StatelessWidget {
     );
   }
 }
+
+// 下面我们介绍一下MaterialPageRoute 构造函数的各个参数的意义：
+
+// MaterialPageRoute({
+//   WidgetBuilder builder,
+//   RouteSettings settings,
+//   bool maintainState = true,
+//   bool fullscreenDialog = false,
+// })
+
+// widget。我们通常要实现此回调，返回新路由的实例。
+// settings 包含路由的配置信息，如路由名称、是否初始路由（首页）。
+// maintainState：默认情况下，当入栈一个新路由时，原来的路由仍然会被保存在内存中，如果想在路由没用的时候释放其所占用的所有资源，可以设置maintainState为false。
+// fullscreenDialog表示新的路由页面是否是一个全屏的模态对话框，在iOS中，如果fullscreenDialog为true，新页面将会从屏幕底部滑入（而不是水平方向）。
