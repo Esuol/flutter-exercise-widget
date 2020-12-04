34f7b58fc8b0e58225889c766d20acf9122f32be
“berlinen” <1292393108@qq.com>
2020-12-04 10:27:17 +0800

    feat: packages get

---
+++ lib/example/package/index.dart
@@ -0,0 +1,15 @@
+// install packages
+
+/// flutter packages get
+///
+/// flutter pub get
+///
+///Running "flutter packages get " in flutter_in_Acion.....
+///Process finished with exit code 0
+///
+///引入 english——words包
+///
+///import 'packega:english_words/english_words.dart'
+///
+///使用 english_words 包来生成随机字符串
+
