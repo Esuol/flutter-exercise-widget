ee36480c4b52fe9bc4db281d4984cb69141c7824
“berlinen” <1292393108@qq.com>
2020-12-04 10:30:36 +0800

    feat: packages pub

--- lib/example/package/index.dart
+++ lib/example/package/index.dart
@@ -1,5 +1,8 @@
 // install packages
 
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
 /// flutter packages get
 ///
 /// flutter pub get
@@ -13,3 +16,14 @@
 ///
 ///使用 english_words 包来生成随机字符串
 
+class RandomWordsWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // 生成随机字符串
+    final wordPair = new WordPair.random();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: new Text(wordPair.toString()),
+    );
+  }
+}
